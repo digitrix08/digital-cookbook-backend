626b65a737ff28aa7831b6da750e6eb14fb4519d
maste <arupkumar_sudhir@srmuniv.edu.in>
2020-04-20 03:24:28 +0530

    validation for invalid email and method to create superuser

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
 
 # Create your models here.
 
@@ -9,15 +9,26 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """ Create and saves new user """
+
+        if not email:
+            raise ValueError
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email=email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save()
         return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     """ Custom user model(supports email instead of username) """
-    email = models.EmailField(null=False, blank=False, max_length=255, unique=True)
+    email = models.EmailField(null=False, blank=False, max_length=255,
+                              unique=True)
     name = models.CharField(max_length=255, null=False, blank=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
